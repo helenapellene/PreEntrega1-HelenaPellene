600143d1065d30608b03d360ed5c334ac9615b32
helenapellene <helenapellene2@gmail.com>
2023-05-30 20:06:51 -0300

    modif titulos

--- carrito.js
+++ carrito.js
@@ -4,7 +4,7 @@ let pant = 0;
 let acc = 0;
 
 while (true) {
-    let comando = prompt("-1) Productos\n-2) Métodos de pago\n-3) Carrito\n-4) Salir");
+    let comando = prompt("-1) PRODUCTOS DISPONIBLES\n-2) METODOS DE PAGO\n-3) VER CARRITO\n-4) SALIR");
 
     if (comando === "salir") {
         break;
@@ -13,10 +13,10 @@ while (true) {
     comando = parseInt(comando);
 
     if (comando === 1) {
-        let lista = parseInt(prompt("Agregar producto al carrito:\n-1) Remeras\n-2) Pantalones\n-3) Accesorios"));
+        let lista = parseInt(prompt("Agregar producto al carrito:\n-1) REMERAS\n-2) PANTALONES\n-3) ACCESORIOS"));
 
         if (lista >= 1 && lista <= 3) {
-            let eleccion = parseInt(prompt(`¿Desea agregar ${lista === 1 ? "'Remeras'" : lista === 2 ? "'Pantalones'" : "'Accesorios'"} al carrito?\n1) Si\n2) No`));
+            let eleccion = parseInt(prompt(`¿Desea agregar ${lista === 1 ? "'REMERAS'" : lista === 2 ? "'PANTALONES'" : "'ACCESORIOS'"} al carrito?\n1) Si\n2) No`));
 
             if (eleccion === 1) {
                 alert("Producto agregado al carrito....");
@@ -36,7 +36,7 @@ while (true) {
             continue;
         }
     } else if (comando === 2) {
-        let metodos = parseInt(prompt("Elija una opcion:\n1) Efectivo\n2) Tarjeta Debito\n3) Tarjeta Credito"));
+        let metodos = parseInt(prompt("Elija una opcion:\n1) EFECTIVO\n2) TARJETA DEBITO\n3) TARJETA CREDITO"));
 
         switch (metodos) {
             case 1:
